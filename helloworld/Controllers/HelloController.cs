c7574512926f51331034c97e5bd7e1104e0cbfc3
Unknown <kbo@e-boks.dk>
2018-07-20 12:50:55 +0200

    hello endpoint

---
+++ helloworld/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("api/hello")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult World(string name)
+        {
+            return this.Ok($"Hello {name}!");
+        }
+    }
+}
